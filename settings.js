41213faf7ca7846a97e4fa8a89e8d91ed14b03eb
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-09-01 08:26:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==wZJdzZ350MINzMYJmbXlzZQFWQrVkRwhDb0UDUyMGSKdnZsNWa6p2N0Q2YjoUSTNEaGZVWpwd=985425647452',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
